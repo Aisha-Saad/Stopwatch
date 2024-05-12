fd0f3273b5825c792291769473cc0ddc78fbb707
Aisha <111194434+Aisha-Saad@users.noreply.github.com>
2024-05-12 10:52:07 +0300

    new upgating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [runing, setRining] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (runing) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!runing) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [runing]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div >
+      <div className="  w-3/2  gap-4 items-center text-xl p-5">
+      <h1> 01- stopwatch</h1>
+      <div className="">
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+
+      <div className=" flex flex-colum items-center text-xl">
+        {runing ? (
+          <button 
+            onClick={() => {
+              setRining(false);
+            }}
+          >
+            Stop
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              setRining(true);
+            }}
+          >
+            Start
+          </button>
+        )}
+
+        <button
+          onClick={() => {
+            setTime(0);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Reset
+        </button>
+      </div>
+    </div>
     </div>
   );
 }
